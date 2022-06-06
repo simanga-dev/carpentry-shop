dae6357f2f689fb830b6d7af03faf9ef3351317f
Hendry <h3ndry@users.noreply.github.com>
2022-06-06 10:17:04 +0200

    bug fix

---
+++ Migrations/20220604214737_sweet.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarpentryShop.Migrations
+{
+    public partial class sweet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isComplete",
+                table: "OrderBoxes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isComplete",
+                table: "OrderBoxes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
