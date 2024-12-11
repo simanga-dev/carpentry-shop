d5e1c8b8cb6ff668510b7849661e29bbf0c3b7dd
Simanga Khoza <simanga-dev@users.noreply.github.com>
2024-12-11 12:38:37 +0200

    feat(validate): make sure that you can only insert numbers.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,65 +5,69 @@
 
 // Handle new item add
 function addRow(type) {
+    $('.datepicker').datepicker('destroy')
 
-    $(".datepicker").datepicker("destroy");
-    
     // Copyable rows
-    let starterTemplate = $('#starter-' + type).html();
-    let count = $('.box-' + type).length;
+    let starterTemplate = $('#starter-' + type).html()
+    let count = $('.box-' + type).length
 
     // Needed to work with asp-for tags
-    let upperCaseType = type.charAt(0).toUpperCase() + type.slice(1) + "s";
+    let upperCaseType = type.charAt(0).toUpperCase() + type.slice(1) + 's'
 
     // Needed to escape special characters
-    RegExp.quote = function(str) {
-        return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
-    };
+    RegExp.quote = function (str) {
+        return str.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1')
+    }
 
-    let regexOne = new RegExp(RegExp.quote(upperCaseType + "_0"), "g");
-    let regexTwo = new RegExp(RegExp.quote(upperCaseType + "[0]"), "g");
-    let newRow = starterTemplate.replace(regexOne, upperCaseType + "_" + count)
-        .replace(regexTwo, upperCaseType + "[" + count + "]");
+    let regexOne = new RegExp(RegExp.quote(upperCaseType + '_0'), 'g')
+    let regexTwo = new RegExp(RegExp.quote(upperCaseType + '[0]'), 'g')
+    let newRow = starterTemplate
+        .replace(regexOne, upperCaseType + '_' + count)
+        .replace(regexTwo, upperCaseType + '[' + count + ']')
 
-    $('#box-' + type).append(newRow);
+    $('#box-' + type).append(newRow)
 
     // Needed to activate radio buttons, if required
-    $('.ui.radio.checkbox').checkbox();
+    $('.ui.radio.checkbox').checkbox()
 
-    showOrHideRemoveButton(type, true);
-    $('.datepicker').removeAttr('id');
-    $('.datepicker').datepicker({ dateFormat: 'yy-mm-dd' });
+    showOrHideRemoveButton(type, true)
+    $('.datepicker').removeAttr('id')
+    $('.datepicker').datepicker({ dateFormat: 'yy-mm-dd' })
 }
 
-
-$('.ui.radio.checkbox').checkbox();
+$('.ui.radio.checkbox').checkbox()
 
 function removeRow(type) {
-    let count = $('.box-' + type).length;
-    let lastItem = $('.box-' + type)[count - 1].remove();
+    let count = $('.box-' + type).length
+    let lastItem = $('.box-' + type)[count - 1].remove()
 
-    if (count === 2) showOrHideRemoveButton(type, false);
+    if (count === 2) showOrHideRemoveButton(type, false)
 }
 
 function showOrHideRemoveButton(type, show) {
-    show ? $('#remove-' + type).show() : $('#remove-' + type).hide();
+    show ? $('#remove-' + type).show() : $('#remove-' + type).hide()
 }
 
-$(function() {
+$(function () {
+    $('.datepicker').datepicker({ dateFormat: 'yy-mm-dd' })
 
-    $('.datepicker').datepicker({ dateFormat: 'yy-mm-dd' });
+    $('.ui.radio.checkboxe').checkbox()
 
-    $('.ui.radio.checkboxe').checkbox();
-
-    $('#generate').click(function() {
-        let valid = $('#boxe-form')[0].checkValidity();
+    $('#generate').click(function () {
+        let valid = $('#boxe-form')[0].checkValidity()
         if (valid) {
-            let element = $(this);
-            element.addClass('loading');
-            setTimeout(function() {
-                element.removeClass('loading');
-            }, 2000);
+            let element = $(this)
+            element.addClass('loading')
+            setTimeout(function () {
+                element.removeClass('loading')
+            }, 2000)
         }
-    });
-});
+    })
+})
 
+function isNumberKey(evt) {
+    var charCode = evt.which ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode !== 46)
+        return false
+    return true
+}
